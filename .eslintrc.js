05e34f542da4f11039f56f91cab5b28b860cb26c
steffen <schoonveld@dataprovider.com>
2021-10-31 20:46:07 +0100

    Boilerplate, eslint config and Chakra.

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: [ "wesbos/typescript" ],
+  rules: {
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": "warn"
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,19 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import * as React from 'react';
+import { ChakraProvider, Container, Flex } from '@chakra-ui/react';
+import Cart from './Cart';
+import Details from './Details';
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
+export const App = (): JSX.Element => (
+  <ChakraProvider>
+    <Container maxWidth="container-xl" padding={0}>
+      <Flex
+        h={{ base: 'auto', md: '100vh' }}
+        py={[0, 10, 20]}
+        direction={{ base: 'column-reverse', md: 'row' }}
+      >
+        <Details />
+        <Cart />
+      </Flex>
+    </Container>
   </ChakraProvider>
-)
+);
